52e79e7e71964b8c325f2e29bf462b22c43ea249
radooo <radoslav.a.tsvetanov.2021@elsys-bg.org>
2026-02-18 15:45:23 +0200

    remade type test

--- tests/core.test.ts
+++ tests/core.test.ts
@@ -3,52 +3,47 @@ import { Blazy } from "../src/core";
 import { tap } from "../src/hooks";
 import z from "zod/v4";
 import type { ClientObject } from "../src/client/Client";
-import { expectTypeOf } from "bun:test";
+import { expectTypeOf, test } from "bun:test";
 
-function processPaymentUsingStripe(v: { amount: string }) {
-    //...
-}
+test("create client returns a correctly typed client", () => {
+    const client = Blazy.create()
+        .http({
+            path: "/koko",
+            handler: v => v,
+            args: z.object({ v: z.string() })
+        })
+        .post({
+            path: "/jiji/koko",
+            handeler: v => v,
+            args: z.object({ koko: z.string() })
+        })
+        .post({
+            path: "/jiji/pllp",
+            handeler: v => v,
+            args: z.object({})
+        })
+        .post({
+            path: "/jiji",
+            handeler: v => v,
+            args: z.object({})
+        })
+        .createClient().createClient();
 
+    type ExpectedRouteTree = {
+        koko: { "/": unknown };
+        jiji: {
+            koko: { "/": unknown };
+            pllp: { "/": unknown };
+            "/": unknown;
+        };
+    };
 
-test create cleint returns a correctly typed cleint, add this as a comment this is to ensure it coirrecctly passes the generateSignedCookie, note that not using the blazy constructor here but instead the static constructor 
+    type Expected = ClientObject<ExpectedRouteTree>;
 
-type expected = // todo
-
-const jg = Blazy.create()
-    .http({
-        path: "/koko/",
-        handler: v => v,
-        args: z.object({ v: z.string() })
-    })
-    .post({
-        path: "/jiji/koko/",
-        handeler: v => v,
-        args: z.object({ koko: z.string() })
-    }) // for now we should pass as const
-    .post({
-        path: "/jiji/pllp/",
-        handeler: v => v,
-        args: z.object({})
-    })
-    .post({
-        path: "/jiji/",
-        handeler: v => v,
-        args: z.object({})
-    }).createClient().createClient()
-
-    expectTypeOf<typeof jg>().toMatchTypeOf<>()
-
-    
-
-
-
-
-
+    expectTypeOf<typeof client>().toMatchTypeOf<Expected>();
+});
 
 
-function jiji<THandler extends (arg: string) => URecord>(config: { name: string, conf: THandler }) {
+function jiji<THandler extends (arg: string) => URecord>(config: { name: string, conf: THandler }) { }
 
-}
-
-
-jiji({ name: "", conf: tap(ctx => { return {} }) })
+jiji({ name: "", conf: tap(ctx => { return {} }) });
